0accf15feac9902fd7650383eff5e4485967b327
nankim <naraekn@gmail.com>
2020-12-26 21:15:24 +0900

    Update AboutMe page
    AboutMe 페이지에 '준비중'이라는 문구를 추가하고
    스타일을 추가했습니다.

--- src/pages/aboutme.tsx
+++ src/pages/aboutme.tsx
@@ -1,6 +1,10 @@
+import { faCoffee } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import React from 'react';
+
 import Layout from '../components/Layout';
 import Navigation from '../components/Navigation/Navigation';
+import './styles/aboutme.scss';
 
 export default function AboutMe() {
   return (
@@ -8,6 +12,12 @@ export default function AboutMe() {
       <div className="index-wrap">
         <Navigation />
         <div className="index-post-list-wrap">
+          <div id="aboutme">
+            <div className="content">
+              준비중 뚝딱뚝딱
+              <FontAwesomeIcon className="icon" icon={faCoffee} />
+            </div>
+          </div>
         </div>
       </div>
     </Layout>
